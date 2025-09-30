c88317a5c4a12b3cda37271bf43cd7d38b8d2c85
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-30 13:35:20 +0530

    initil setup for Rezorpay project

---
+++ Razorpay/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+        fontFamily: {
+            mullish: ["Mulish", "sans-serif"]
+        },
+        colors: {
+            deepBlue: "#02042a",
+            lightBlue: "#2b84ed",
+            lightBlue300: "#4b94ed",
+            lightBlue500: "#0b72e7",
+            greenLight: "#61cea6",
+            grayText: "#818597",
+            lightGray: "#33a6c",
+            deepVlueHead: "#162f56",
+            gray2: "#525a76",
+        },
+    },
+  },
+  plugins: [],
+}
